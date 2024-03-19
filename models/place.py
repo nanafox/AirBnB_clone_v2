871183a73b3db00360dce14e7d72d14244b51c41
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-19 14:48:54 +0000

    minor syntax changes on line 36 and line 40

--- models/place.py
+++ models/place.py
@@ -28,23 +28,20 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-
     place_amenity = Table('place_amenity', metadata,
-            Column('place_id', String(60),
-                ForeignKey('place.id'),
-                primary_key=True,nullable=False)),
+        Column('place_id', String(60),
+            ForeignKey('place.id'),
+            primary_key=True, nullable=False),
+        Column('amenity_id', String(60),
+            ForeignKey('amenities.id'),
+            primary_key=True, nullable=False))
 
-            Column('amenity_id', String(60),
-                    ForeignKey('amenities.id'),
-                    primary_key=True, nullable=False))
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review",
+                backref="place", cascade="all, delete")
+        amenities = relationship("Amenity",
+                secondary="place_amenity",viewonly=False)
 
-            if getenv("HBNB_TYPE_STORAGE") == "db":
-                reviews = relationship(
-                        "Review", backref="place", cascade="all, delete"
-                        )
-                amenities = relationship(
-                        "Amenity", secondary="place_amenity",viewonly=False
-                        )
     else:
 
         @property
@@ -58,7 +55,6 @@ class Place(BaseModel, Base):
             return review_list
 
         @property
-        """ setting the getter """
         def amenities(self):
             """ Returns the list of Amenity instances"""
 
