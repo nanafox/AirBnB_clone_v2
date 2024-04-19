9c03737274e5dec7eae7c6450e3c4a8b2f66662b
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-19 09:17:34 +0000

    Enable whitespace controls

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,8 @@ from models import storage
 from models.state import State
 
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
