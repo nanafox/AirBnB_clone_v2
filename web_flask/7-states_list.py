eabc3e04293d473bc6fc51527f1da860e639124c
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-19 09:04:54 +0000

    Add Flask route for displaying a list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""This script starts the flask application and runs the web server."""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(_):
+    """Closes the current session after each request."""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def get_states():
+    """Renders all the State objects available in the storage."""
+    states = storage.all(State).values()
+
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
