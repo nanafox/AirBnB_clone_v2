6fff4476dfb842eeb05c59643260bacf39348cab
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-20 11:48:30 +0000

    Fix SQLAlchemyError handling in DBStorage.reload()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,6 +4,7 @@
 
 from os import getenv
 from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 from models.city import City
@@ -26,7 +27,7 @@ class DBStorage:
             "mysql+mysqldb://"
             f"{getenv('HBNB_MYSQL_USER')}:{getenv('HBNB_MYSQL_PWD')}"
             f"@{getenv('HBNB_MYSQL_HOST')}/{getenv('HBNB_MYSQL_DB')}",
-            pool_pre_ping=True,
+            pool_pre_ping=True
         )
 
         if getenv("HBNB_ENV") == "test":
@@ -63,7 +64,11 @@ class DBStorage:
     def reload(self):
         """Creates all tables in the database and the current
         database session."""
-        Base.metadata.create_all(self.__engine)
+        try:
+            Base.metadata.create_all(self.__engine)
+        except SQLAlchemyError:
+            pass
+
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False
         )
