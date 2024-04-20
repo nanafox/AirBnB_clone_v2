b0266d1cce609b700f916e0973761636eae36d52
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-20 15:43:37 +0000

    Add set_default_collation method to LazyMethods class in lazy_methods.py

--- lazy_methods.py
+++ lazy_methods.py
@@ -3,9 +3,11 @@
 """This module defines useful short methods that are heavily used during
 testing."""
 
+from os import getenv
 from io import StringIO
 from uuid import UUID as uuid
 from random import choice
+import MySQLdb
 
 
 class LazyMethods:
@@ -69,3 +71,70 @@ class LazyMethods:
         key = choice(list(self.__random_attributes.keys()))
 
         return key, choice(self.__random_attributes[key])
+
+    @staticmethod
+    def set_default_collation(charset="utf8mb4", collate="utf8mb4_unicode_ci"):
+        """
+        Sets the default collation for the MySQL database and all its tables.
+
+        Args:
+            charset (str, optional): The character set to be used. Defaults to
+            "utf8mb4".
+            collate (str, optional): The collation to be used. Defaults to
+            "utf8mb4_unicode_ci".
+
+        This method establishes a connection to the MySQL database, retrieves
+        all table names, and sets the default collation for the database and
+        each table to the specified charset and collate values. It also
+        temporarily disables foreign key checks during the process.
+
+        Note:
+            This method requires the following environment variables to be set:
+            - HBNB_MYSQL_DB: The name of the MySQL database
+            - HBNB_MYSQL_USER: The username for the MySQL database
+            - HBNB_MYSQL_PWD: The password for the MySQL database
+            - HBNB_MYSQL_HOST: The host address for the MySQL database
+        """
+        # Establish a database connection
+        db = MySQLdb.connect(
+            db=f"{getenv('HBNB_MYSQL_DB')}",
+            user=f"{getenv('HBNB_MYSQL_USER')}",
+            password=f"{getenv('HBNB_MYSQL_PWD')}",
+            host=f"{getenv('HBNB_MYSQL_HOST')}",
+        )
+
+        # Create a cursor object
+        cursor = db.cursor()
+
+        # Execute SQL command to get all table names
+        cursor.execute("SHOW TABLES")
+
+        # Fetch all the rows
+        tables = cursor.fetchall()
+
+        cursor.execute("SET FOREIGN_KEY_CHECKS=0")
+
+        cursor.execute(
+            f"ALTER DATABASE {getenv('HBNB_MYSQL_DB')} "
+            f"CHARACTER SET = {charset} COLLATE = {collate};"
+        )
+
+        for table_name in tables:
+            # Execute SQL command to alter table character set
+            table_name = table_name[0]
+
+            try:
+                cursor.execute(
+                    f"ALTER TABLE {table_name} "
+                    f"CONVERT TO CHARACTER SET {charset} COLLATE {collate}"
+                )
+            except MySQLdb.ProgrammingError:
+                pass
+
+        cursor.execute("SET FOREIGN_KEY_CHECKS=1")
+
+        # Commit your changes in the database
+        db.commit()
+
+        # Disconnect from server
+        db.close()
